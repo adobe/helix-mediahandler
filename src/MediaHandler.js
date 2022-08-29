83cf611966c8abea890e7cefaf2b94459624ba98
Stefan Guggisberg <sguggisb@adobe.com>
2022-08-29 18:58:31 +0200

    feat: R2 mirror

--- src/MediaHandler.js
+++ src/MediaHandler.js
@@ -12,7 +12,7 @@
 /* eslint-disable no-plusplus,no-param-reassign */
 
 import crypto from 'crypto';
-import { Transform } from 'stream';
+import { PassThrough, Transform } from 'stream';
 
 import { context, h1, timeoutSignal } from '@adobe/helix-fetch';
 import mime from 'mime';
@@ -48,6 +48,9 @@ export default class MediaHandler {
       _awsRegion: opts.awsRegion || process.env.AWS_S3_REGION,
       _awsAccessKeyId: opts.awsAccessKeyId || process.env.AWS_S3_ACCESS_KEY_ID,
       _awsSecretAccessKey: opts.awsSecretAccessKey || process.env.AWS_S3_SECRET_ACCESS_KEY,
+      _r2AccountId: opts.r2AccountId || process.env.CLOUDFLARE_ACCOUNT_ID,
+      _r2AccessKeyId: opts.r2AccessKeyId || process.env.CLOUDFLARE_R2_ACCESS_KEY_ID,
+      _r2SecretAccessKey: opts.r2SecretAccessKey || process.env.CLOUDFLARE_R2_SECRET_ACCESS_KEY,
       _bucketId: opts.bucketId || 'helix-media-bus',
       _contentBusId: opts.contentBusId,
       _owner: opts.owner,
@@ -101,6 +104,14 @@ export default class MediaHandler {
       this._log.info('Creating S3Client without credentials');
       this._s3 = new S3Client();
     }
+    this._log.info('Creating R2 S3Client');
+    this._r2 = new S3Client({
+      endpoint: `https://${this._r2AccountId}.r2.cloudflarestorage.com`,
+      credentials: {
+        accessKeyId: this._r2AccessKeyId,
+        secretAccessKey: this._r2SecretAccessKey,
+      },
+    });
 
     this.fetchContext = fetchDefaultContext;
     // eslint-disable-next-line no-constant-condition
@@ -151,7 +162,7 @@ export default class MediaHandler {
   }
 
   /**
-   * Creates an external resource from the given buffer and properties.
+   * Creates an external resource from the given stream and properties.
    * @param {Readable} stream - readable stream
    * @param {number} [contentLength] - Size of blob.
    * @param {string} [contentType] - content type
@@ -162,17 +173,43 @@ export default class MediaHandler {
     if (!contentLength) {
       throw Error('createExternalResourceFromStream() needs contentLength');
     }
-    // ensure readable
-    await new Promise((resolve, reject) => {
-      stream.once('readable', resolve);
+    // in order to compute hash, we need to read at least 8192 bytes
+    const partialBuffer = await new Promise((resolve, reject) => {
+      const chunks = [];
+      let read = 0;
+      const readMax = Math.min(contentLength, 8192);
+
+      const done = () => {
+        // eslint-disable-next-line no-use-before-define
+        stream.removeListener('readable', onReadable);
+        // eslint-disable-next-line no-use-before-define
+        stream.removeListener('end', onEnd);
+        const buf = Buffer.concat(chunks);
+        stream.unshift(buf);
+        resolve(buf);
+      };
+      const onEnd = () => {
+        done();
+      };
+      const onReadable = () => {
+        let chunk;
+        // eslint-disable-next-line yoda, no-cond-assign
+        while (null !== (chunk = stream.read())) {
+          chunks.push(chunk);
+          read += chunk.length;
+          if (read > readMax) {
+            done();
+            break;
+          }
+        }
+      };
+
+      stream.on('readable', onReadable);
+      stream.on('end', onEnd);
       stream.on('error', (e) => {
         reject(Error(`Error reading stream: ${e.code}`));
       });
     });
-
-    // in order to compute hash, we need to read at least 8192 bytes
-    const partialBuffer = stream.read(Math.min(contentLength, 8192));
-    stream.unshift(partialBuffer);
 
     // compute hash
     const resource = this._initMediaResource(partialBuffer, contentLength);
@@ -394,19 +431,29 @@ export default class MediaHandler {
   async putMetaData(blob) {
     const { log } = this;
     const c = requestCounter++;
-    try {
-      log.debug(`[${c}] COPY ${blob.storageUri}`);
-      await this._s3.send(new CopyObjectCommand({
-        Bucket: this._bucketId,
-        Key: blob.storageKey,
-        CopySource: `${this._bucketId}/${blob.storageKey}`,
-        Metadata: blob.meta,
-        MetadataDirective: 'REPLACE',
-      }));
+
+    const cmd = new CopyObjectCommand({
+      Bucket: this._bucketId,
+      Key: blob.storageKey,
+      CopySource: `${this._bucketId}/${blob.storageKey}`,
+      Metadata: blob.meta,
+      MetadataDirective: 'REPLACE',
+    });
+    log.debug(`[${c}] COPY ${blob.storageUri}`);
+    // send cmd to s3 and r2 (mirror) in parallel
+    const result = await Promise.allSettled([
+      this._s3.send(cmd),
+      this._r2.send(cmd),
+    ]);
+    const rejected = result.filter(({ status }) => status === 'rejected');
+    if (!rejected.length) {
       log.info(`[${c}] Metadata updated for: ${blob.storageUri}`);
       MediaHandler.updateBlobURI(blob);
-    } catch (e) {
-      log.info(`[${c}] Failed to update metadata for ${blob.storageUri}: ${e.$metadata.httpStatusCode || e.message}`);
+    } else {
+      // at least 1 cmd failed
+      const type = result[0].status === 'rejected' ? 'S3' : 'R2';
+      const e = rejected[0].reason;
+      log.info(`[${c}] [${type}] Failed to update metadata for ${blob.storageUri}: ${e.$metadata.httpStatusCode || e.message}`);
     }
   }
 
@@ -505,7 +552,7 @@ export default class MediaHandler {
           blob.meta.height = height;
         }
       } else {
-        // create transfer stream and store the first mb
+        // create transform stream and store the first mb
         const capture = new Transform({
           transform(chunk, encoding, callback) {
             /* istanbul ignore next */
@@ -521,27 +568,55 @@ export default class MediaHandler {
       }
     }
 
-    const upload = new Upload({
+    const params = {
+      Bucket: this._bucketId,
+      Key: blob.storageKey,
+      Body: blob.data || blob.stream,
+      ContentType: blob.contentType,
+      Metadata: blob.meta,
+    };
+
+    let s3Body;
+    let r2Body;
+    if (!Buffer.isBuffer(params.Body)) {
+      // Body is a stream:
+      const stream = params.Body;
+      // need to create separate readable streams for s3 and r2
+      s3Body = new PassThrough();
+      r2Body = new PassThrough();
+      stream.pipe(s3Body);
+      stream.pipe(r2Body);
+    } else {
+      // Body is a buffer
+      s3Body = params.Body;
+      r2Body = params.Body;
+    }
+    const s3Upload = new Upload({
       client: this._s3,
-      params: {
-        Bucket: this._bucketId,
-        Key: blob.storageKey,
-        Body: blob.data || blob.stream,
-        ContentType: blob.contentType,
-        Metadata: blob.meta,
-      },
+      params: { ...params, Body: s3Body },
+    });
+    const r2Upload = new Upload({
+      client: this._r2,
+      params: { ...params, Body: r2Body },
     });
 
-    try {
-      const result = await upload.done();
-      log.info(`[${c}] Upload done ${blob.storageKey}: ${result.Location}`);
-    } catch (e) {
-      log.error(`[${c}] Failed to upload blob ${blob.storageKey}: ${e.status || e.message}`);
+    // upload to s3 and r2 (mirror) in parallel
+    const result = await Promise.allSettled([
+      s3Upload.done(),
+      r2Upload.done(),
+    ]);
+    const rejected = result.filter(({ status }) => status === 'rejected');
+    // discard data
+    delete blob.stream;
+    delete blob.data;
+    if (!rejected.length) {
+      log.info(`[${c}] Upload done ${blob.storageKey}: ${result[0].value.Location}`);
+    } else {
+      // at least 1 cmd failed
+      const type = result[0].status === 'rejected' ? 'S3' : 'R2';
+      const e = rejected[0].reason;
+      log.error(`[${c}] [${type}]: Failed to upload blob ${blob.storageKey}: ${e.status || e.message}`);
       return false;
-    } finally {
-      // discard data
-      delete blob.stream;
-      delete blob.data;
     }
 
     // check if we need to update the metadata with the dimensions
